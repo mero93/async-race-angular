357ea210af4eece28bba1d161e28beadf470fd4b
mero93 <merabkopaleishvili1993@gmail.com>
2026-07-22 08:35:00 +0400

    feat: implement responsive header component

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,26 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideCarFront, LucideMenu, LucideTrophy } from '@lucide/angular';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmDialogImports } from '@spartan-ng/helm/dialog';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    HlmButtonImports,
+    HlmDialogImports,
+    NgOptimizedImage,
+    LucideMenu,
+    LucideTrophy,
+    LucideCarFront,
+  ],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  host: {
+    class:
+      'block w-full border-b border-border/40 bg-secondary/95 backdrop-blur supports-[backdrop-filter]:bg-secondary/60 sticky top-0 z-50',
+  },
 })
 export class Header {}
