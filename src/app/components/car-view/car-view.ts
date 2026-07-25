489ca10cde09d3cf0d0a22a7c588a681a5e77d40
mero93 <merabkopaleishvili1993@gmail.com>
2026-07-25 04:53:43 +0400

    feat: add car view component

---
+++ src/app/components/car-view/car-view.ts
@@ -0,0 +1,30 @@
+import { Component, computed, input } from '@angular/core';
+
+import { Car, CarState } from '../../types/car';
+
+@Component({
+  selector: 'app-car-view',
+  standalone: true,
+  templateUrl: './car-view.html',
+  host: {
+    class: 'block w-full max-w-sm',
+  },
+})
+export class CarView {
+  readonly car = input.required<Car>();
+  readonly carState = input<CarState>();
+
+  readonly driving = input<boolean>();
+
+  readonly isDriving = computed(() => {
+    const manualOverride = this.driving();
+    if (manualOverride !== undefined) {
+      return manualOverride;
+    }
+
+    const status = this.carState()?.status;
+    return status === 'driving' || status === 'started';
+  });
+
+  readonly carColor = computed(() => this.car().color || '#ef4444');
+}
